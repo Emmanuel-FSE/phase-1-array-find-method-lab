d9932ba395abbe6484dc1ef27b3521324a1bb2ba
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2022-11-30 21:50:09 +0300

    Asignment Complete

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 const record = [
-    { year: "2015", result: "W"},
-    { year: "2014", result: "N/A"},
-    { year: "2013", result: "L"},
-    //...
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  //...
   ]
-
-function superbowlWin(obj, index, array){
-    let year
-    if(obj.result === "W"){
-         year =  obj.year;
-    } 
-
-    return year;
-}
-
-const year = record.find(superbowlWin).year;
-
-console.log(year);
+  
+  function superbowlWin(obj){
+  let year;
+  obj.find(function(obj){
+  if(obj.result === "W"){
+  year = obj.year;
+  } 
+  });
+  return year;
+  }
