080ec88902a83aa15404eb43a49d29b76b48b32f
emmanuelmutisya254@gmail.com <EmmanuelTheWebDeveloper>
2022-11-30 17:43:22 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+function superbowlWin(obj, index, array){
+    let year
+    if(obj.result === "W"){
+         year =  obj.year;
+    } 
+
+    return year;
+}
+
+const year = record.find(superbowlWin).year;
+
+console.log(year);
